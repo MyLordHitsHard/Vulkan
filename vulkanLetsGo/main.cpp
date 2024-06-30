5bf0ef8ed999f8efae21dff22567b1b6b005a441
Himanshu Dutt <37894863+MyLordHitsHard@users.noreply.github.com>
2024-06-30 23:57:39 +0530

    add: glfwInit call

--- vulkanLetsGo/main.cpp
+++ vulkanLetsGo/main.cpp
@@ -25,6 +25,7 @@ private:
     GLFWwindow* window;
 
     void initWindow() {
+        glfwInit();
         glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
         glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
         window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
